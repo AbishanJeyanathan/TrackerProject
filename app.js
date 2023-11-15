77d5401e13fee9ea21990c97103f03d87f7c0a60
Abishan Jeyanathan <abishanjeyanathan@gmail.com>
2023-11-14 23:40:37 -0500

    Assignment

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 // Imports
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require("mongoose");
+const session = require("express-session");
+const app = express();
+const port = 3000;
 
-// Static Files
-app.use(express.static('public'));
-// Specific folder example
-// app.use('/css', express.static(__dirname + 'public/css'))
-// app.use('/js', express.static(__dirname + 'public/js'))
-// app.use('/img', express.static(__dirname + 'public/images'))
+// database connection
+(async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/Assignment3", { useNewUrlParser: true });
+    console.log('Successfully Connected to MongoDB');
 
-// Set View's
-app.set('views', './views');
-app.set('view engine', 'ejs');
+    const db = mongoose.connection;
+    db.on('error', (error) => console.log(error));
+    db.once('open', () => console.log('Successfully Connected to the Database'));
 
-// Navigation
-app.get('', (req, res) => {
-    res.render('index', { text: 'Hey' })
-})
+    // Static Files
+    app.use(express.static('public'));
+    // Specific folder example
+    app.use('/css', express.static(__dirname + '/public/css'));
+    app.use('/js', express.static(__dirname + '/public/js'));
+    app.use('/img', express.static(__dirname + '/public/images'));
 
-app.get('/about', (req, res) => {
-   res.sendFile(__dirname + '/views/about.html')
-})
+    // Set View's
+    app.set('views', './views');
+    app.set('view engine', 'ejs');
 
-app.listen(port, () => console.info(`App listening on port ${port}`))
+    // Navigation
+    app.get('', (req, res) => {
+      res.render('home', { text: 'Hey' });
+    });
+
+    app.get('/about', (req, res) => {
+      res.sendFile(__dirname + '/views/about.html');
+    });
+
+    app.listen(port, () => {
+      console.log(`Server started at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Error Connecting to MongoDB:', error.message);
+  }
+})();
