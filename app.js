b159c89704b1ed03f7d33e0b55ec0a74c4d1f146
Abishan Jeyanathan <abishanjeyanathan@gmail.com>
2023-11-15 15:24:25 -0500

    Assignment3

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 // Imports
 const express = require('express');
 const mongoose = require("mongoose");
+const conncectDB = require('./db');
 const session = require("express-session");
 const app = express();
+const controller = require('./controllers/controller')
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require( 'body-parser' );
 const port = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/userInfo', { useNewUrlParser: true, useUnifiedTopology: true });
-const personSchema = new mongoose.Schema({
-    Playername: String,
-    Playerheight: Number,
-    Playerposition: String,
-});
-const Person = mongoose.model('Person', personSchema);
+connectDB();
 
 // db.js
 const connectDB = async () => {
@@ -33,41 +29,9 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.post('/addPerson', (req, res) => {
-  const { Playername, Playerheight, Playerposition } = req.body;
-
-  if (!Playername || !Playerheight || !Playerposition) {
-      return res.status(400).json({ error: 'Name, height, and position are required' });
-  }
-
-  const newPerson = new Person({
-      name: Playername,
-      height: Playerheight,
-      position: Playerposition,
-  });
-
-  newPerson.save((err) => {
-      if (err) {
-          res.status(500).send(err);
-      } else {
-          res.redirect('/people');
-      }
-  });
-});
-
-app.get('/people', (req, res) => {
-  Person.find({}, (err, people) => {
-      if (err) {
-          res.status(500).send(err);
-      } else {
-          res.render('people', { people: people });
-      }
-  });
-});
+app.get('/', controller.getIndex);
+app.post('/addPerson', controller.addperson);
+app.get('/people', controller.addperson);
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
